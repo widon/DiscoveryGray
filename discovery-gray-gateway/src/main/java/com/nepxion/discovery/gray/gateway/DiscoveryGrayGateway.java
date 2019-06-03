1221d5b25424e12cd83dd056d28669994d690bd8
Widon Han <widon.han@cignacmb.com>
2019-06-03 16:12:45 +0800

    在官网demo中替换consul 和apollo

--- discovery-gray-gateway/src/main/java/com/nepxion/discovery/gray/gateway/DiscoveryGrayGateway.java
+++ discovery-gray-gateway/src/main/java/com/nepxion/discovery/gray/gateway/DiscoveryGrayGateway.java
@@ -14,10 +14,12 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
+import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
 import com.nepxion.discovery.plugin.strategy.gateway.filter.GatewayStrategyRouteFilter;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableApolloConfig("application")
 public class DiscoveryGrayGateway {
     public static void main(String[] args) {
         new SpringApplicationBuilder(DiscoveryGrayGateway.class).run(args);
